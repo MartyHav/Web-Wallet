55a1b6dc790ca47302f768822e5ac136c4fdb77f
Neil Coggins <ncoggins@earth.localdomain>
2019-06-07 14:28:08 +0100

    added initial examples of RPC calls

--- src/components/_layout/menu/index.js
+++ src/components/_layout/menu/index.js
@@ -4,14 +4,55 @@ import React, { Component } from "react";
 // Relative Imports
 import { Container, Overview, Item, Value, Wrapper, Amount } from "./styles";
 
+import RPC from "../../rpc";
+
+
 class Menu extends Component {
+
+    state = {
+        dCurrentBalance : 0.0,
+        error : ""
+    };
+    
+  getBalance = () => {    
+      
+      // NEAC - parameters to configure for a call to the backend RPC code
+      var strMethod = "get_balance";
+      var jsonParams = JSON.stringify({"account_index":0,"address_index":[0]});
+
+      RPC.call_rpc("https://nelliekins.zapto.org", strMethod, jsonParams)
+          .then(function(objResponse) {
+              debugger;
+              if (objResponse.hasOwnProperty("result")) {
+
+                  // Got a balance - display it somehow
+                  debugger;
+                  this.setState({dCurrentBalance: new Number(objResponse.result.balance / 1000000000000).toFixed(12)});
+              }
+          }.bind(this))
+          .catch(function(myError) {
+              this.setState({ error: myError.message });
+              setTimeout(() => {
+                  this.setState({ error: "" });
+              }, 2000);
+          }.bind(this));
+  }
+    
   render() {
+
+      const { dCurrentBalance } = this.state;
+      
+      // Trigger a call to getBalance()
+      setTimeout(() => {
+          this.getBalance({ error: "" });
+      }, 5000);
+      
     return (
       <Container>
         <Overview>
           <Wrapper>
-            <Amount>$1,234.56</Amount>
-            <Value>Total Balance (USD)</Value>
+            <Amount>{dCurrentBalance}</Amount>
+            <Value>Total Balance (XHV)</Value>
           </Wrapper>
         </Overview>
 
---
+++ src/components/rpc/index.js
@@ -0,0 +1,37 @@
+// Library Imports
+import React from "react";
+
+class RPC extends React.Component {
+
+    static call_rpc(strURL, strMethod, jsonParams) {
+        
+        // NEAC - make a call to the RPC code
+        return window.fetch(strURL + "/api.php/v1/" + strMethod, {
+            method:"POST",
+            mode:"cors",
+            cache:"no-cache",
+            credentials: 'omit',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+            referrer: 'no-referrer',
+            body: jsonParams
+        })
+            .then(function(response) {
+                if (response.ok) {
+                    return response.text();
+                }
+                throw new Error("invalid response");
+            }.bind(this))
+            .then(function(jsonResponse) {
+                return JSON.parse(jsonResponse);
+            }.bind(this));
+    };
+
+    render() {
+        return "";
+    };
+};
+
+export default RPC;
